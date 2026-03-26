0df506b3b4b592795cc559974c9b57b1577bb7c7
fgaume <fgaume@gmail.com>
2026-03-26 20:17:32 +0100

    fix: further reduce donut chart height to 290px

--- src/components/EffectifsDonut.tsx
+++ src/components/EffectifsDonut.tsx
@@ -167,7 +167,7 @@ export function EffectifsDonut({ effectifs, difficulties, requestedCount, newLyc
       aria-label={`Répartition des ${total} places de seconde entre ${effectifs.length} lycées de secteur 1`}
     >
       <div className="effectifs-donut-chart">
-        <ResponsiveContainer width="100%" height={320}>
+        <ResponsiveContainer width="100%" height={290}>
           <PieChart>
             <Pie
               data={data}
